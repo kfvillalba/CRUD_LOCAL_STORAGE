956afb52070f2686b4374031e346fa0993d22366
kfvillalba <kfvillalba@unicesar.edu.co>
2022-04-13 12:53:37 -0500

    s

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const formularioUI = document.getElementById('formulario');
 const listaProductosUI = document.getElementById('listaProductos');
+const listaProductosCarritoUI = document.getElementById('listaProductosCarrito');
 let arrayProductos = [];
 let arrayCarrito = [];
 
@@ -29,6 +30,70 @@ const GuardarProductosDB = () =>{
     LeerProductosDB();
 }
 
+const GuardarProductosCarritoDB = () =>{
+    localStorage.setItem('Carrito', JSON.stringify(arrayCarrito));
+    
+    LeerProductosCarritoDB();
+}
+
+
+const EliminarProductosDB = (codProducto) =>{
+    let indexArray = arrayProductos.findIndex((producto)=>producto.codProducto === codProducto)
+    arrayProductos.splice(indexArray,1);
+    GuardarProductosDB();
+}
+
+
+const EliminarProductosCarritoDB = (codProducto) =>{
+    let indexArray = arrayCarrito.findIndex((producto)=>producto.codProducto === codProducto)
+    arrayCarrito.splice(indexArray,1);
+    arrayProductos[indexArray].stockProducto++;
+    GuardarProductosCarritoDB();
+}
+
+const LeerProductosCarritoDB = () => {
+    
+    listaProductosUI.innerHTML += '<h3 class="my-4" id="lista">CARRITO</h3>';
+
+    arrayCarrito = JSON.parse(localStorage.getItem('Carrito'));
+
+    if(arrayCarrito == null){
+        arrayProductos = [];
+    }else{     
+        arrayCarrito.forEach(element => {
+            let customStatus;
+            element.stockProducto == 0 ? customStatus = 'danger': customStatus = 'success'; 
+            listaProductosCarritoUI.innerHTML += `<div class="alert alert-${customStatus}" role="alert">
+            <i class="material-icons float-start me-3">
+                shopping_bag
+            </i>
+            <b>${element.codProducto}</b>  
+            <span class="float-end">
+                <i class="material-icons">
+                    shopping_cart
+                </i>  
+                <i class="material-icons">
+                    delete
+                </i>                   
+            </span> 
+            <span class="float-start me-5">
+                ${element.nombreProducto} 
+            </span>  
+
+            <span class="float-end me-5">
+                ${element.stockProducto} 
+            </span>
+
+            <span class="float-end me-5">
+                ${element.valorProducto}  
+            </span> 
+
+          </div>`            
+        });
+    }   
+
+}
+
 const LeerProductosDB = () =>{    
     listaProductosUI.innerHTML = '';
     listaProductosUI.innerHTML += '<h3 class="my-4" id="lista">LISTA DE PRODUCTOS</h3>'; 
@@ -41,7 +106,7 @@ const LeerProductosDB = () =>{
     }else{     
         arrayProductos.forEach(element => {
             let customStatus;
-            element.estado === false ? customStatus = 'danger': customStatus = 'success'; 
+            element.stockProducto == 0 ? customStatus = 'danger': customStatus = 'success'; 
             listaProductosUI.innerHTML += `<div class="alert alert-${customStatus}" role="alert">
             <i class="material-icons float-start me-3">
                 shopping_bag
@@ -71,16 +136,11 @@ const LeerProductosDB = () =>{
         });
     }
 }
-
-const EliminarProductosDB = (codProducto) =>{
+const ComprarProductosDB = (codProducto) => {
     let indexArray = arrayProductos.findIndex((producto)=>producto.codProducto === codProducto)
-    arrayProductos.splice(indexArray,1);
-    GuardarProductosDB();
-}
-const EditarProductosDB = (codProducto) => {
-    let indexArray = arrayProductos.findIndex((producto)=>producto.codProducto === codProducto)
-    arrayProductos[indexArray].estado = true;
-    console.log(indexArray);
+    arrayProductos[indexArray].stockProducto --;
+    let item = arrayProductos[indexArray];
+    arrayCarrito.push(item);    
     GuardarProductosDB();
 }
 
@@ -100,15 +160,20 @@ formularioUI.addEventListener('submit', (e) => {
 
     GuardarProductosDB();
 })
+const Leer = () => {
+   
+    LeerProductosDB();
+    LeerProductosCarritoDB();
+}
 
-document.addEventListener('DOMContentLoaded', LeerProductosDB());
+document.addEventListener('DOMContentLoaded',Leer());
 
 listaProductosUI.addEventListener('click', (e) =>{
     e.preventDefault();
     let texto =  e.path[2].childNodes[3].innerHTML;
      
     if (e.target.innerHTML.trim() == 'shopping_cart') {
-        EditarProductosDB(texto);
+        ComprarProductosDB(texto);
    }else if( e.target.innerHTML.trim() == 'delete'){       
        EliminarProductosDB(texto);
    }
